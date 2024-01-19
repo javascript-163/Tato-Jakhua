3340ef6c6381c1fa0abf6f1f4620df93f39457b9
tato jakhua <anatoli.jakhua@gmail.com>
2024-01-20 00:01:17 +0400

    first home work finished

--- workshop1/script.js
+++ workshop1/script.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+let firstName = prompt("what is your name?");
+
+let lastName = prompt("what is your lastName?");
+
+let address = prompt("what is your current location?");
+
+const userAge = 2024 - prompt("Your date of birth?");
+
+alert(
+  "გამარჯობა " +
+    firstName +
+    " " +
+    lastName +
+    ", მიხარია, რომ " +
+    userAge +
+    " წლის ხარ და " +
+    address +
+    "-ში ცხოვრობ."
+);
